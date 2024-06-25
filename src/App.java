f2cce6a99fb6515962de203e606291b7182013d9
MicheleDelvecchio <delvecchiomichele1999@gmail.com>
2024-06-25 15:07:35 +0200

    commit

--- src/App.java
+++ src/App.java
@@ -8,4 +8,8 @@ public class App {
     public static void test() {
         System.out.println("Hello World!");
     }
+
+    public static void test2() {
+        System.out.println("Ciao Mondo!");
+    }
 }
